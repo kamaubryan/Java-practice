b906887c1878d64bdca1cf6369b09b65ddb8dae5
kamaubryan <briankamau344@gmail.com>
2024-10-27 23:20:20 +0300

    intro to java

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+// they are divided into two : primitive and non-primitive data types
+// primitive datatypes are later divided into four: integer,float,character,boolean
+public class DataTypes {
+    public static void main(String[] args) {
+        int kakpo = 34;// 4 bytes
+        System.out.println(kakpo);
+        long jdcf = 45565;// 8 bytes
+        System.out.println(jdcf);
+        short krek = 454;// 2 bytes
+        System.out.println(krek);
+        char c = 'k'; // while assigning a character you have to use the single speech marks not like in string where we use double ones
+        System.out.println(c);
+        c++;// you can increment a character like a number
+        System.out.println(c);// the output here wil be l. it follows the alphabetical order
+        boolean factual =true; //only true or false
+        System.out.println(factual);
+        double achievements = 5.6;// you have to specify the type
+        System.out.println(achievements);
+        float loops = 5.5f;// in float, you have to indicate if it is a double or float . if it is a float you put the letter f at the end
+        System.out.println(loops);
+    }
+}
---
+++ variables.java
@@ -0,0 +1,14 @@
+// variables are where we store our data. it behaves like a box where we store our data
+// how we create variablees and how we execute them
+
+public class variables {
+    public static void main(String[] args) {
+        // the normal way of executing our code we can use the sout method
+        System.out.println(3+4);// we have executed the data inside a print statement. print ln means it will print in another line
+        // to avoid the above we can first store the data then execute
+        int num1 = 24;// you have to specify the type of the value
+        int num2 = 54;
+        int result = num2+num1; // here we have use the add operator
+        System.out.println("the result is equals to"+" "+result);
+    }
+}
